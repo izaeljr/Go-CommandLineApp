b70d69b20078cde1aeeef2277e6e6aae1a1825da
izaeljr <jucb05@gmail.com>
2024-02-05 23:38:36 -0300

    feat: Add search servers

--- app/app.go
+++ app/app.go
@@ -14,17 +14,25 @@ func Generate() *cli.App {
 	app.Name = "Command Line Application"
 	app.Usage = "Search IP's and server names around the internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "github.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search IP's addresses on internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "github.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search IP's addresses on internet",
+			Flags:  flags,
 			Action: searchIps,
+		},
+		{
+			Name:   "server",
+			Usage:  "Search server names on internet",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 
@@ -42,5 +50,19 @@ func searchIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, exception := net.LookupNS(host)
+
+	if exception != nil {
+		log.Fatal(exception)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
